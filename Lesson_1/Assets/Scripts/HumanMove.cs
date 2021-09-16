b39b9ea70cfa1d87dcc353baadb7c7c1373ed977
Olexandr <rebus1335@gmail.com>
2021-09-16 13:13:14 +0300

    Add first lesson

---
+++ Lesson_1/Assets/Scripts/HumanMove.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HumanMove : MonoBehaviour
+{
+    [SerializeField]
+     private float _speed;
+     public GameObject door;
+     public bool isGround ;
+     
+    
+    
+
+    
+
+   void OnCollisionEnter(Collision other)
+    {
+       if(other.gameObject.layer == 8)
+       
+       isGround = true;
+    }
+    void  FixedUpdate()
+    {
+        if(isGround)
+        {
+          door.transform.position = new Vector3(16, 5, -49);
+        }
+        
+      if(Input.GetKey(KeyCode.W))
+    {
+        transform.position += Vector3.forward*_speed*Time.deltaTime;
+        transform.rotation = Quaternion.Euler(0,0,0);
+    }
+    else if(Input.GetKey(KeyCode.S))
+    {
+        transform.position -= Vector3.forward*_speed*Time.deltaTime;
+        transform.rotation = Quaternion.Euler(0,180,0);
+    }   
+     else if(Input.GetKey(KeyCode.A))
+    {
+        transform.position += Vector3.left*_speed*Time.deltaTime;
+        transform.rotation = Quaternion.Euler(0,-90,0);
+    }
+    else if(Input.GetKey(KeyCode.D))
+    {
+        transform.position -= Vector3.left*_speed*Time.deltaTime;
+        transform.rotation = Quaternion.Euler(0,90,0);
+    }   
+    }
+    
+  
+   
+}
